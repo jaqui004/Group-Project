a3f53ccc54ec5065a82dc00f8ee9a26f07bc9250
jaqui004 <jaqui004@odu.edu>
2023-03-17 18:24:04 -0400

    section update

--- src/edu/odu/cs/cs350/pne/Section.java
+++ src/edu/odu/cs/cs350/pne/Section.java
@@ -10,13 +10,67 @@ public class Section {
 	private Course secCour;
 	private String CRN;
 	private int xListCap;
-	// xListGroup?
+	private int xListGroup;
 	private int enrolled;
 	private int enrollmentCap;
-	private String location;
+	// ENR
+	// Link
+	private String building;
+	private String room;
 	private Vector<Snapshot> snapshotVector= new Vector<>();
+	// Schedule type
+	// Campus 
+	// INSM
+	// Print
+	private String meetTime;
+	private String meetDays;
+	// Override
+	private String instructor;
+	private String ptrmStart;
+	private String ptrmEnd;
+	private int waitListCap;
+	private int waitList;
+	private int waitListRemain;
 
+	// Section constructor
+	// Read in order:
+	// enrolled, CRN, subject, 
+	// Part of course
+		// course number, title, credits, hours, 
+	// xListCap,
+	// ENR?, link, schedulteType, campus, INSM, print?, meetTime, meetDays, 
+	// building, room, override?, instructor, overallCap, ptrmStart,
+	// ptrmEnd, waitListCap, waitList, waitListRemain
+	public Section(int enrollInput, String crnInput, Course courInput, 
+	              int xCapInput /*, ENR, link, scheduleType, */
+				  String campInput/*INSM, print*/, String meetTimeInput,
+				  String meetDaysInput, String buildInput,
+				  String roomInput,/*override*/ String instructorInput,
+				  String ptrmStartInput, String ptrmEndInput,
+				  int waitCapInput, int waitInput, int waitRemainInput){
 
+		enrolled = enrollInput;
+		CRN = crnInput;
+		secCour = courInput;
+		xlistCap = xCapInput;
+		// ENR
+		// Link
+		// scheduletype
+		campus = campInput;
+		// INSM
+		// print
+		meetTime = meetTimeInput;
+		meetDays = meetDaysInput;
+		building = buildInput
+		room = roomInput;
+		// override
+		instructor = instructorInput;
+		ptrmStart = ptrmStartInput;
+		ptrmEnd = ptrmEndInput;
+		waitListCap = waitCapInput;
+		waitList = waitInput;
+		waitListRemain = waitRemainInput;
+	}
 
 	// Accessor functions
 
