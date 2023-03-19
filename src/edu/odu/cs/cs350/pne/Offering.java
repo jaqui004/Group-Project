35f98a6c49c5d4b22331d9c42d14c9451dd1e762
jaqui004 <jaqui004@odu.edu>
2023-03-18 20:05:18 -0400

    Update offering

--- src/edu/odu/cs/cs350/pne/Offering.java
+++ src/edu/odu/cs/cs350/pne/Offering.java
@@ -8,7 +8,7 @@ public class Offering {
 	// Made up of a course, a list of sections, an overall enrollment cap, 
 	// an overall enrollment number, and a cross list group
 	private Course courOff;
-	private Vector<Section> sectionVector = new Vector<>();
+	private Vector<Section> sectionVector = new Vector<Section>();
 	private int overEnrollCap;
 	private int overEnrolled;
 	// xListGroup?
