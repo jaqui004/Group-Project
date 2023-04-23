3382bda60de25e6b02b69e0d7cf527a96c84fad5
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-23 15:08:23 -0400

    fix

--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -1,15 +1,23 @@
-package test.edu.odu.cs.cs350.pne;
+package edu.odu.cs.cs350.pne;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class TestCourse{
-    
 
+    //default values
+    String defaultSubj="CS";
+    String defaultCourseNum = "120G";
+
+   
     @Test
-    public void testCourseName() {
-        // Test to output the course name in a single string.
-        String subj;
-        System.out.println("This is the the subject: " + subj);
-    
+    public void testCourseConstructor() {
+        Course course1= new Course(defaultSubj, defaultCourseNum);
+        Course crs= new Course();
+        assertEquals(course1.getSubj(), defaultSubj);
+        assertEquals(course1.getCourNum(), defaultCourseNum);
+        assertNotEquals(course1 , crs);
+
     }
 }
