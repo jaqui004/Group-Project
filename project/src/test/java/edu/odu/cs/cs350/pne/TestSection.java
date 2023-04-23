514f98437f2734c8b87bf9bc6495c30cfb070f32
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-23 06:10:03 -0400

    test cases for section

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -4,21 +4,25 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
-import java.util.Vector;
+//import java.util.ArrayList;
+import java.util.LinkedList;
 
-import org.junit.jupiter.api.Test;
+
 
 public class TestSection{
     //setup default values
-    public Course defaultCourse = new Course();
     public String defaultCRN = "11592";
-    public int defualtXlistCap = 50;
+    public int defaultXlistCap = 50;
     public int defaultXlistGroup = 30;
     public int defaultEnrolled = 0 ;
-    // **Dont forget to initialize vector!**
-	public Vector <Snapshot> defaultSnapshots = new Vector <Snapshot>();
+    public String defaultSemesterDir = "202010";
+    public String defaultDate = "2020-04-01";
+    public Course defaultCourse = new Course("CS","120G");
+    // **Dont forget to initialize List!**
+	public LinkedList <Snapshot> defaultSnapshots = new LinkedList <Snapshot>();
     public String defaultLink = "P1";
     public String defaultBuilding = "OCNPS", defaultRoom = "0100";
+    public Snapshot defaultSnaps = new Snapshot(defaultSemesterDir, defaultDate);
     public File defaultEnrollSnap = new File("./project/src/test/data/202010/2020-04-01");
     
     
@@ -32,13 +36,56 @@ public class TestSection{
         assertEquals(section1.getXListCap(), 0);
         assertEquals(section1.getXListGroup(), 0);
         assertEquals(section1.getEnrolled(), 0);
-        assertTrue(section1.getSnapshotVec().isEmpty());
+        assertTrue(section1.getSnapshotList().isEmpty());
         assertEquals(section1.getLink(), "");
         assertEquals(section1.getBuilding(), "");
         assertEquals(section1.getRoom(), "");
        /// assertEquals(section1.getEnrollmentAsOf("", ""),);
 
     }
+
+    @Test
+    public void testSectionCopyConst(){
+        Section section1 = new Section(defaultSemesterDir,defaultDate);
+        String tmp = " 0 - ";
+
+        assertEquals(section1.getCourse().toString(), tmp);
+        assertEquals(section1.getCRN(), "");
+        assertEquals(section1.getXListCap(), 0);
+        assertEquals(section1.getXListGroup(), 0);
+        assertEquals(section1.getEnrolled(), 0);
+        assertFalse(section1.getSnapshotList().isEmpty());
+        assertEquals(section1.getLink(), "");
+        assertEquals(section1.getBuilding(), "");
+        assertEquals(section1.getRoom(), "");
+       /// assertEquals(section1.getEnrollmentAsOf("", ""),);
+
+    }
+
+    @Test
+    public void testSectionCopyConst3(){
+        defaultSnapshots.add(defaultSnaps);
+        Section section1 = new Section(defaultCRN,defaultCourse,defaultXlistCap,
+        defaultEnrolled,defaultLink,defaultXlistGroup,defaultBuilding, 
+        defaultRoom,defaultSnapshots);
+        //String tmp = " 0 - ";
+
+        assertEquals(section1.getCourse(), defaultCourse);
+        assertEquals(section1.getCRN(), defaultCRN);
+        assertEquals(section1.getXListCap(), defaultXlistCap);
+        assertEquals(section1.getXListGroup(), defaultXlistGroup);
+        assertEquals(section1.getEnrolled(), defaultEnrolled);
+
+        assertTrue(section1.getSnapshotList().contains(defaultSnaps));
+        assertFalse(section1.getSnapshotList().isEmpty());
+        assertTrue(section1.getSnapshotList().equals(defaultSnapshots));
+
+        assertEquals(section1.getLink(), defaultLink);
+        assertEquals(section1.getBuilding(), defaultBuilding);
+        assertEquals(section1.getRoom(), defaultRoom);
+    }
+
+
 
     @Test
     public void testAddSnapshot(){
@@ -48,10 +95,29 @@ public class TestSection{
         String tmpDir = "202010";
         String dateTmp = "2020-04-01";
         
-        assertTrue(section1.getSnapshotVec().contains(snp1));
-        assertEquals(section1.getSnapshotVec().size(), 1);
+        assertTrue(section1.getSnapshotList().contains(snp1));
+        assertEquals(section1.getSnapshotList().size(), 1);
         
-        assertEquals(section1.getEnrollmentAsOf(tmpDir, dateTmp), snp1);
+        assertEquals(section1.getEnrollmentAsOf(tmpDir, dateTmp) , snp1);
+
+
+    }
+
+    @Test
+    public void testReads(){
+        Section section1 = new Section();
+        Snapshot snaps1 = new Snapshot("202010", "2020-04-01");
+
+        section1.read_SectionRecords(snaps1); 
+        //assertEquals(section1.getCourse(), defaultCourse);
+        assertEquals(section1.getCRN(), defaultCRN);
+        assertEquals(section1.getXListCap(), defaultXlistCap);
+        assertEquals(section1.getXListGroup(), defaultXlistGroup);
+        assertEquals(section1.getEnrolled(), defaultEnrolled);
+        assertNotEquals(section1.getSnapshotList(), 0);
+        assertEquals(section1.getLink(), defaultLink);
+        assertEquals(section1.getBuilding(), defaultBuilding);
+        assertEquals(section1.getRoom(), defaultRoom);
 
 
     }
