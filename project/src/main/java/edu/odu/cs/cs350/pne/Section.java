d6e0e1e42dc9086eff942acecff97be1d22fc386
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-23 14:46:59 -0400

    file fixes

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -1,5 +1,9 @@
 package edu.odu.cs.cs350.pne;
-import java.util.Vector;
+
+import java.io.File;
+import java.util.LinkedList;
+//import java.util.Vector;
+import java.util.Scanner;
 
 public class Section {
 	
@@ -14,155 +18,195 @@ public class Section {
 	private int xListCap;
 	private int xListGroup;
 	private int enrolled;
-	private int enrollmentCap;
-	// ENR
-	// Link
+	private String link;
 	private String building;
 	private String room;
-	private Vector<Snapshot> snapshotVector= new Vector<Snapshot>();
-	// Schedule type
-	private String campus; 
-	// INSM
-	// Print
-	private String meetTime;
-	private String meetDays;
-	// Override
-	private String instructor;
-	private String ptrmStart;
-	private String ptrmEnd;
-	private int waitListCap;
-	private int waitList;
-	private int waitListRemain;
-
-
-
+	private LinkedList<Snapshot> snapshotList= new LinkedList<Snapshot>();
+	
+	
 
 
 	// Section constructor
-	// Read in order:
-	// enrolled, CRN, subject, 
-	// Part of course
-	// course number, title, credits, hours, 
-	// xListCap,
-	// ENR?, link, schedulteType, campus, INSM, print?, meetTime, meetDays, 
-	// building, room, override?, instructor, overallCap, ptrmStart,
-	// ptrmEnd, waitListCap, waitList, waitListRemain
-	public Section(int enrollInput, String crnInput, Course courInput, 
-	              int xCapInput, int xlistCap, /*, ENR, link, scheduleType, */
-				  String campInput/*INSM, print*/, String meetTimeInput,
-				  String meetDaysInput, String buildInput,
-				  String roomInput,/*override*/ String instructorInput,
-				  String ptrmStartInput, String ptrmEndInput,
-				  int waitCapInput, int waitInput, int waitRemainInput){
-
-		enrolled = enrollInput;
-		CRN = crnInput;
-		secCour = courInput;
-		xlistCap = xCapInput;
-		// ENR
-		// Link
-		// scheduletype
-		campus = campInput;
-		// INSM
-		// print
-		meetTime = meetTimeInput;
-		meetDays = meetDaysInput;
-		building = buildInput;
-		room = roomInput;
-		// override
-		instructor = instructorInput;
-		ptrmStart = ptrmStartInput;
-		ptrmEnd = ptrmEndInput;
-		waitListCap = waitCapInput;
-		waitList = waitInput;
-		waitListRemain = waitRemainInput;
+	public Section(){
+		secCour = new Course();
+		CRN = "";
+		xListCap = 0;
+		xListGroup = 0;
+		enrolled = 0;
+		link = "";
+		building = "";
+		room = "";
+		snapshotList = new LinkedList <Snapshot>();
 	}
 
+	public Section(String semestDir, String dateExct ){
+		snapshotList = new LinkedList <Snapshot>();
+		snapshotList.add(new Snapshot(semestDir, dateExct));
+		secCour = new Course();
+		CRN = "";
+		xListCap = 0;
+		xListGroup = 0;
+		enrolled = 0;
+		link = "";
+		building = "";
+		room = "";
+	}
 
-
+	public Section(String CRN2, Course crse, int xListCap2, int enroll, String link2,
+		int xListGroup2, String building2, String room2, LinkedList<Snapshot> snapE){
+				CRN=CRN2;
+				secCour=crse;
+				xListCap=xListCap2;
+				enrolled=enroll;
+				link=link2;
+				xListGroup= xListGroup2;
+				building=building2;
+				room=room2;
+				for(Snapshot tmp : snapE ){
+					snapshotList.add(tmp);
+				}
+		}
 
 
 	// Accessor functions
-	public Course getCourse(){return secCour.clone();}
+	public Course getCourse(){return secCour;}
 	public String getCRN(){return CRN;}
 	public int getXListCap(){return xListCap;}
 	public int getXListGroup(){return xListGroup;}
 	public int getEnrolled(){return enrolled;}
-	public int getEnrollmentCap(){return enrollmentCap;}
-	public int getWaitListCap(){return waitListCap;}
-	public int getWaitList(){return waitList;}
-	public int getWaitRemain(){return waitListRemain;}
-	
-	// ENR?
-	// Link??
+	public String getLink(){return link;}
 	public String getBuilding(){return building;}
 	public String getRoom(){return room;}
-	// Vector
-	// Schedule type
-	public String getCampus(){return campus;}
-	// INSM
-	// Print
-	public String getMeetTime(){return meetTime;}
-	public String getMeetDays(){return meetDays;}
-	// Override
-	public String getInstructor(){return instructor;}
-	public String getPTRMStart(){return ptrmStart;}
-	public String getPTRMEnd(){return ptrmEnd;}
-	//public String getWaitListCap(){return waitListCap;}
-	// public String getWaitList(){return waitList;}
-	// public String getWaitRemain(){return waitRemain;}
+	public LinkedList <Snapshot>  getSnapshotList(){return snapshotList;}
 
+	/**
+	 * Get the snapshot for a certain date
+	 * @param dateDir the semesterCode
+	 * @param date the date yyyy-mm-dd format
+	 */
+	public  Snapshot getEnrollmentAsOf(String dateDir, String date){
+		
+		//loop through list of snapshots and return if found else return null
+		
+		for (Snapshot snapEn : this.getSnapshotList()){
+				if(snapEn.getSemesterDirectory()== dateDir
+				&& snapEn.getDate()== date)
+					return snapEn;		
+			
+		}
+		return null;
+		
 
-
-
+	}
 
 	// Mutator functions
 
-
-
-
-
-	// Adds a snapshot to this sections vector of snapshots
+	// Adds a snapshot to this sections list of snapshots
 	public void addSnapshot(Snapshot snapIn){
-		snapshotVector.add(snapIn.clone());
+		snapshotList.add(snapIn);
 	}
 
-
+	/**
+	 * Read in values making up section
+	 * @param snapsSht -the certain snapshot which to read from
+	 */
+	public void read_SectionRecords(Snapshot snapsSht){
+		//use scanner to read and initialze records
 		
+		Scanner x;
+		boolean found = false;
+		int y =0;
+		int count =0;
+		
+		
+		//Setup exception handler
+		try
+		{
+			x = new Scanner(new File(snapsSht.getTargetFile().getPath()));
+			//x.useDelimiter("[,]");
+		
+			
+			while(x.hasNext() && !found)
+			{
+				
+				String tmp=x.nextLine();
+				Scanner strScan = new Scanner(tmp).useDelimiter(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
 
-	// Creates a clone of a section
-	public Section clone(){
-		// Create new section object with variables
-		Section secClone = new Section(enrolled, CRN, secCour.clone(), 
-						   xListCap, /*ENR, link, scheduletype, */
-						   campus, /*INSM, print,*/ meetTime, meetDays,
-						   building, room, /*override,*/ instructor,
-						   ptrmStart, ptrmEnd, waitListCap, waitList,
-						   waitListRemain);
+				while (strScan.hasNext()){
+					String token = strScan.next().trim().replaceAll("\"", "");
+					CRN = token;
+				}
+				strScan.close();
+				//String tmp=x.next().trim().replaceAll("\"","");
+				// Scanner tmpScan = new Scanner(tmp).useDelimiter("");
+				//move count up when value has been read stop once all have been read
+				while(count!=7){
+					//String tmp=x.next().trim().replaceAll("\"","");
+					//conditions for when at certian positions
+					//for CRN
+					if(y==1){
+						this.CRN= tmp;
+						count++;
+					}
+					//course subj
+					if(y==2){
+						this.secCour.setSubj(tmp);
+						count++;	
+					}
+					//course number
+					if(y==3){
+						this.secCour.setCourNum(tmp);
+						count++;
+					}
+					
+				
+				}
+			}
+			
+			x.close();
+		}
+		
+		catch(Exception e)
+		{
 
-		// Make a copy of the snapshot vector and uses a loop to fill it
-		// with snapshots
-		Vector<Snapshot> snapVecClone = new Vector<Snapshot>();
-		for(int i = 0; i < snapshotVector.size(); i++){
-			snapVecClone.add(snapshotVector.get(i).clone());
 		}
 
-		// return clone
-		return secClone;
 	}
 
-	// Prints out information of the section
-	
-    public void testOutPut(){ // i changed the name of this because we are moving it to TestSection
-							// and it was causing an annoying error	
-		System.out.println(secCour.toString() + " - CRN " + CRN); 
-		System.out.println("Instructor: " + instructor + ", Location: " + campus + " campus, " + building + " " + room);
-	    System.out.println("Meeting Time: " + meetDays + " " + meetTime);
-		System.out.println("Start Date: " + ptrmStart + ", End Date: " + ptrmEnd); 
-		System.out.println("Currently Enrolled: " + enrolled + ", Enrollment Cap: " + enrollmentCap + ", xList Group: " + xListGroup + ", xList Cap: " + xListCap);
-		System.out.println("Wait List Cap: " + waitListCap + ", Wait List: " + waitList + ", Wait List Remaining: " + waitListRemain);
-	}
-	
+
+
+	// Creates a clone of a section
+	// public Section clone(){
+	// 	// Create new section object with variables
+		 
+	// 	//(enrolled, CRN, secCour.clone(), 
+	// 	// 				   xListCap, /*ENR, link, scheduletype, */
+	// 	// 				   campus, /*INSM, print,*/ meetTime, meetDays,
+	// 	// 				   building, room, /*override,*/ instructor,
+	// 	// 				   ptrmStart, ptrmEnd, waitListCap, waitList,
+	// 	// 				   waitListRemain);
+
+	// 	// // Make a copy of the snapshot vector and uses a loop to fill it
+	// 	// // with snapshots
+	// 	// Vector<Snapshot> snapVecClone = new Vector<Snapshot>();
+	// 	// for(int i = 0; i < snapshotVector.size(); i++){
+	// 	// 	snapVecClone.add(snapshotVector.get(i).clone());
+	// 	// }
+
+	// 	// return clone
+	// 	return secClone;
+	// }
+
+	// // Prints out information of the section
+	// public String toString(){
+	// 	return (secCour.toString() + " - CRN " + CRN 
+	// 		 + "Instructor: " + instructor + ", Location: " + campus + " campus, " 
+	// 		 + building + " " + room + " "
+	// 		 + "Meeting Time: " + meetDays + " " + meetTime + " "
+	// 		 + "Start Date: " + ptrmStart + ", End Date: " + ptrmEnd + " " 
+	// 		 + "Currently Enrolled: " + enrolled + ", Enrollment Cap: " + enrollmentCap
+	// 		 + ", xList Group: " + xListGroup + ", xList Cap: " + xListCap + " "
+	// 		 + "Wait List Cap: " + waitListCap + ", Wait List: " 
+	// 		 + waitList + ", Wait List Remaining: " + waitListRemain + " ");
+	// }
 }
-	
-
--- project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -1,11 +1,125 @@
 package edu.odu.cs.cs350.pne;
 
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+//import java.util.ArrayList;
+import java.util.LinkedList;
+
+
 
 public class TestSection{
+    //setup default values
+    public String defaultCRN = "11592";
+    public int defaultXlistCap = 50;
+    public int defaultXlistGroup = 30;
+    public int defaultEnrolled = 0 ;
+    public String defaultSemesterDir = "202010";
+    public String defaultDate = "2020-04-01";
+    public Course defaultCourse = new Course("CS","120G");
+    // **Dont forget to initialize List!**
+	public LinkedList <Snapshot> defaultSnapshots = new LinkedList <Snapshot>();
+    public String defaultLink = "P1";
+    public String defaultBuilding = "OCNPS", defaultRoom = "0100";
+    public Snapshot defaultSnaps = new Snapshot(defaultSemesterDir, defaultDate);
+    public File defaultEnrollSnap = new File("./project/src/test/data/202010/2020-04-01");
+    
     
     @Test
-    public void tetsSectionConstructor(){
-        
+    public void testSectionConstructor(){
+        Section section1 = new Section();
+        String tmp = " 0 - ";
+
+        assertEquals(section1.getCourse().toString(), tmp);
+        assertEquals(section1.getCRN(), "");
+        assertEquals(section1.getXListCap(), 0);
+        assertEquals(section1.getXListGroup(), 0);
+        assertEquals(section1.getEnrolled(), 0);
+        assertTrue(section1.getSnapshotList().isEmpty());
+        assertEquals(section1.getLink(), "");
+        assertEquals(section1.getBuilding(), "");
+        assertEquals(section1.getRoom(), "");
+       /// assertEquals(section1.getEnrollmentAsOf("", ""),);
+
     }
+
+    @Test
+    public void testSectionCopyConst(){
+        Section section1 = new Section(defaultSemesterDir,defaultDate);
+        String tmp = " 0 - ";
+
+        assertEquals(section1.getCourse().toString(), tmp);
+        assertEquals(section1.getCRN(), "");
+        assertEquals(section1.getXListCap(), 0);
+        assertEquals(section1.getXListGroup(), 0);
+        assertEquals(section1.getEnrolled(), 0);
+        assertFalse(section1.getSnapshotList().isEmpty());
+        assertEquals(section1.getLink(), "");
+        assertEquals(section1.getBuilding(), "");
+        assertEquals(section1.getRoom(), "");
+       /// assertEquals(section1.getEnrollmentAsOf("", ""),);
+
+    }
+
+    @Test
+    public void testSectionCopyConst3(){
+        defaultSnapshots.add(defaultSnaps);
+        Section section1 = new Section(defaultCRN,defaultCourse,defaultXlistCap,
+        defaultEnrolled,defaultLink,defaultXlistGroup,defaultBuilding, 
+        defaultRoom,defaultSnapshots);
+        //String tmp = " 0 - ";
+
+        assertEquals(section1.getCourse(), defaultCourse);
+        assertEquals(section1.getCRN(), defaultCRN);
+        assertEquals(section1.getXListCap(), defaultXlistCap);
+        assertEquals(section1.getXListGroup(), defaultXlistGroup);
+        assertEquals(section1.getEnrolled(), defaultEnrolled);
+
+        assertTrue(section1.getSnapshotList().contains(defaultSnaps));
+        assertFalse(section1.getSnapshotList().isEmpty());
+        assertTrue(section1.getSnapshotList().equals(defaultSnapshots));
+
+        assertEquals(section1.getLink(), defaultLink);
+        assertEquals(section1.getBuilding(), defaultBuilding);
+        assertEquals(section1.getRoom(), defaultRoom);
+    }
+
+
+
+    @Test
+    public void testAddSnapshot(){
+        Section section1 = new Section();
+        Snapshot snp1 = new Snapshot("202010", "2020-04-01");
+        section1.addSnapshot(snp1);
+        String tmpDir = "202010";
+        String dateTmp = "2020-04-01";
+        
+        assertTrue(section1.getSnapshotList().contains(snp1));
+        assertEquals(section1.getSnapshotList().size(), 1);
+        
+        assertEquals(section1.getEnrollmentAsOf(tmpDir, dateTmp) , snp1);
+
+
+    }
+
+    @Test
+    public void testReads(){
+        Section section1 = new Section();
+        Snapshot snaps1 = new Snapshot("202010", "2020-04-01");
+
+        section1.read_SectionRecords(snaps1); 
+        //assertEquals(section1.getCourse(), defaultCourse);
+        assertEquals(section1.getCRN(), defaultCRN);
+        assertEquals(section1.getXListCap(), defaultXlistCap);
+        assertEquals(section1.getXListGroup(), defaultXlistGroup);
+        assertEquals(section1.getEnrolled(), defaultEnrolled);
+        assertNotEquals(section1.getSnapshotList(), 0);
+        assertEquals(section1.getLink(), defaultLink);
+        assertEquals(section1.getBuilding(), defaultBuilding);
+        assertEquals(section1.getRoom(), defaultRoom);
+
+
+    }
+
 }
