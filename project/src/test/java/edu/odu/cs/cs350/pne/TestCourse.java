94c9033a210361c1fb695615a04c1fec31fd0384
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-23 05:33:43 -0400

    test the course constructor

--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -1,15 +1,29 @@
 package edu.odu.cs.cs350.pne;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class TestCourse{
     
+    //default values
+    String defaultSubj="CS";
+    String defaultCourseNum = "120G";
+
 
     @Test
-    public void testCourseName() {
-        // Test to output the course name in a single string.
-
-        System.out.println("This is the the subject: ");
+    public void testCourseConstructor() {
+        Course course1= new Course(defaultSubj, defaultCourseNum);
+        Course crs= new Course();
+        assertEquals(course1.getSubj(), defaultSubj);
+        assertEquals(course1.getCourNum(), defaultCourseNum);
+        assertNotEquals(course1 , crs);
     
     }
 }
