03a8c38e3ff37a46d30df5162817f52541df0e6c
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-23 15:07:45 -0400

    test course fix

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class TestSnapshot{
     //set default values for testing
