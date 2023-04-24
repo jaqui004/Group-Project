40e4e0863fb6b6b76a714b99ddcc87ce3386f532
RodQ10 <Rodrigoquinonez23@gmail.com>
2023-04-24 13:35:09 -0400

    make tests for semester

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
@@ -1,12 +1,29 @@
 package edu.odu.cs.cs350.pne;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.LinkedList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
 public class TestSemester{
+   public String defaultCode = "202010";
     
-@Test
-public void testSemesterConstructor(){
+    @Test
+    public void testSemesterConstructor(){
+    Semester semester1 = new Semester();
 
-}
+    assertNotEquals(semester1.getSemCode(), defaultCode);
+    assertThat(semester1.getOfferingslist(), is(new LinkedList<Offering>()));
     
-}
+    
+    }
+
+    @Test 
+    public void testAddOffers(){
+        
+    }
+    
+    }
